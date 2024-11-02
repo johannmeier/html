090e04ed002c3fb22c6e8a7a618c4f16482f28b6
Johann Meier <johann.meier3@yahoo.de>
2024-11-02 18:44:13 +0100

    ok cancel dialog; error, warn and info dialog

--- src/js/modalDialog.js
+++ src/js/modalDialog.js
@@ -1,66 +1,104 @@
-window.closeDialog = function (event) {
-    let rect = event.target.getBoundingClientRect();
-    if (rect.left > event.clientX ||
-        rect.right < event.clientX ||
-        rect.top > event.clientY ||
-        rect.bottom < event.clientY
-    ) {
-        event.target.close();
+window.dialog = (function () {
+    function closeDialog(event) {
+        let rect = event.target.getBoundingClientRect();
+        if (rect.left > event.clientX ||
+            rect.right < event.clientX ||
+            rect.top > event.clientY ||
+            rect.bottom < event.clientY
+        ) {
+            event.target.close();
+        }
     }
-}
 
-window.showDialog = function (content) {
-    let host = document.getElementById("dialogDiv")
-    if (!host) {
-        host = document.createElement("div");
-        host.id = "dialogDiv"
-        document.body.appendChild(host);
-        const shadow = host.attachShadow({mode: 'open'});
-        shadow.innerHTML = window.getDialogHtml(`<span>${content}</span>`)
+    function showOkCancelDialog(content, okCallback) {
+        const dialog = createHostAndDialog("okCancelDialogDiv", `<span>${content}</span>`,
+            (dialog) => {
+                const buttonDiv = document.createElement("div");
+                buttonDiv.style.marginTop = "5px";
+                buttonDiv.style.display = "flex";
+                buttonDiv.style.justifyContent = "center";
+                buttonDiv.style.gap = "5px";
+                buttonDiv.innerHTML = "<button form='dialog-form' value='ok'>Ok</button><button form='dialog-form' value='cancel'>Cancel</button>";
+                dialog.appendChild(buttonDiv);
+
+                dialog.addEventListener("close", () => {
+                    if ('ok' === dialog.returnValue && okCallback) {
+                        okCallback();
+                    }
+                });
+            });
+        dialog.showModal();
     }
-    let dialog = host.shadowRoot.querySelector("dialog")
-    dialog.showModal();
-}
 
-window.showOkCancelDialog = function (content, callback) {
+    function showDialog(content, type) {
+        let map = new Map([
+            ["error", `<img style="float: left; height: 30px" alt="Error" src="/img/error.svg"/>`],
+            ["warning", `<img style="float: left; height: 30px" alt="Warning" src="/img/warning.svg"/>`],
+            ["info", `<img style="float: left; height: 30px" alt="Warning" src="/img/info.svg"/>`]
+        ]);
 
-}
+        let img = map.get(type);
+        if (!img) {
+            img = "";
+        }
 
-window.showIframeDialog = function (url) {
-    let host = document.getElementById("iframeDialogDiv")
-    if (!host) {
-        host = document.createElement("div");
-        window.resizeIframe = function(iframe) {
+        createHostAndDialog("dialogDiv",
+            `${img}<span>${content}</span>`
+        ).showModal();
+    }
+
+    function showIframeDialog(url) {
+        function resizeIframe(event) {
+            const iframe = event.currentTarget;
             let x = 5;
             iframe.height = iframe.contentWindow.document.documentElement.scrollHeight + x + "px";
             iframe.width = iframe.contentWindow.document.documentElement.scrollWidth + x + "px";
         }
-        host.id = "iframeDialogDiv"
-        document.body.appendChild(host);
-        const shadow = host.attachShadow({mode: 'open'});
-        shadow.innerHTML = window.getDialogHtml(`<iframe style="border:none" onload="window.resizeIframe(this)"></iframe>`)
+
+        const dialog = createHostAndDialog("iframeDialogDiv", `<iframe style="border:none"</iframe>`,
+            (dialog) => {
+                dialog.querySelector("iframe").addEventListener("load", resizeIframe);
+                dialog.style.padding = "0";
+            })
+        dialog.querySelector("iframe").src = url;
+        dialog.showModal();
     }
 
-    let dialog = host.shadowRoot.querySelector("dialog")
-    dialog.showModal();
-    dialog.querySelector("iframe").src = url;
-}
+    function createHostAndDialog(hostId, content, createdCallback) {
+        let host = document.getElementById(hostId)
+        if (!host) {
+            host = document.createElement("div");
+            host.id = hostId;
+            document.body.appendChild(host);
+            const shadow = host.attachShadow({mode: 'open'});
+            shadow.innerHTML = getDialogHtml(content);
+            const dialog = shadow.querySelector("dialog");
+            dialog.addEventListener("click", closeDialog);
+            if (createdCallback) {
+                createdCallback(dialog)
+            }
+        }
+        return host.shadowRoot.querySelector("dialog");
+    }
 
-window.showAjaxDialog = function (params) {
-
-}
-
-window.getDialogHtml = function (contentElement) {
-    return `<style>
+    function getDialogHtml(contentElement) {
+        return `<style>
              dialog[open] {
                  background-color: aquamarine;
                  min-width: 200px;
              }
          </style>
-         <dialog style="padding:0" onclick="closeDialog.apply(this, arguments)">
-             <form method="dialog">
+         <dialog style="padding: 5px">
+             <form method="dialog" id="dialog-form">
                  <button style="position:absolute;right:0;top:0;font-size:x-small;padding:0" type="submit">✖️</button>
              </form>
              ${contentElement}
          </dialog>`;
-}
+    }
+
+    return {
+        "showDialog": showDialog,
+        "showOkCancelDialog": showOkCancelDialog,
+        "showIframeDialog": showIframeDialog
+    }
+})();
