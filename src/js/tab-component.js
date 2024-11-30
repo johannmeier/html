eb7976a21f72a78b5cdabd50b8e7dab48388d70b
Johann Meier <johann.meier3@yahoo.de>
2024-11-30 08:45:09 +0100

    tab-component, working

--- src/js/tab-component.js
+++ src/js/tab-component.js
@@ -1,10 +1,14 @@
 (() => {
-    function TabComponent(element) {
-        let currentFocus = 1;
+    function TabComponent(component) {
+        let currentX = 1;
+        let currentY = 1;
+        let type;
 
-        function focus(x) {
-            const currentElement = document.querySelector(`[data-tab-x='${x}']`);
+        function focus(x, y) {
+            const currentElement = getElement(x, y);
             if (currentElement) {
+                component.querySelectorAll('[data-tab-x], [data-tab-y]').forEach(e => e.tabIndex = -1);
+                currentElement.tabIndex = 0;
                 setTimeout(() => {
                     currentElement.focus();
                 }, 0);
@@ -14,39 +18,67 @@
             }
         }
 
-        function wire() {
-            element.addEventListener('keydown', function (event) {
-                if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
-                    if (currentFocus > 0) {
-                        if (focus(currentFocus - 1)) {
-                            currentFocus--;
-                        }
-                    }
-                    event.preventDefault();
+        function getElement(x, y) {
+            if (type === "xy") {
+                return component.querySelector(`[data-tab-x='${x}'][data-tab-y='${y}']`);
+            }
+            if (type === "x") {
+                return component.querySelector(`[data-tab-x='${x}']`);
+            }
+            if (type === "y") {
+                return component.querySelector(`[data-tab-y='${y}']`);
+            }
+
+            return null;
+        }
+
+        function setType() {
+            if (component.querySelector("[data-tab-x][data-tab-y]")) {
+                type = "xy";
+            } else if (component.querySelector("[data-tab-x]")) {
+                type = "x";
+            } else {
+                type = "y";
+            }
+        }
+
+        const keys = new Map();
+        keys.set(
+            'ArrowUp', () => {
+                if (focus(currentX, currentY - 1)) {
+                    currentY--;
                 }
-                if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
-                    if (focus(currentFocus + 1)) {
-                        currentFocus++;
-                    }
-                    event.preventDefault();
+            });
+        keys.set(
+            'ArrowLeft', () => {
+                if (focus(currentX - 1, currentY)) {
+                    currentX--;
+                }
+            });
+        keys.set(
+            'ArrowDown', () => {
+                if (focus(currentX, currentY + 1)) {
+                    currentY++;
+                }
+            });
+        keys.set(
+            'ArrowRight', () => {
+                if (focus(currentX + 1, currentY)) {
+                    currentX++;
                 }
             });
 
-            element.addEventListener('focus', function (event) {
-                if (!event.relatedTarget.dataset.tabX) {
-                    element.tabIndex = -1;
-                    focus(currentFocus);
-                }
-            });
-
-            element.addEventListener('blur', function (event) {
-                if (event.relatedTarget.dataset.tabX) {
-                    element.tabIndex = 0;
+        function wireXY() {
+            component.addEventListener('keydown', function (event) {
+                if (keys.has(event.key)) {
+                    keys.get(event.key)();
+                    event.preventDefault();
                 }
             });
         }
 
-        wire();
+        setType();
+        wireXY();
     }
 
     function wire() {
